0fc2f97b19f3e0b85322bb5a0cb4b696bb9126c2
koreahghg <koreahghg@gmail.com>
2026-04-02 09:48:53 +0900

    .

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++
@@ -1,6 +0,0 @@
-function solution(num, k) {
-    const str = num.toString();
-    const idx = str.indexOf(k.toString());
-    
-    return idx === -1 ? -1 : idx + 1;
-}
--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++
@@ -1,7 +0,0 @@
-function solution(my_string, k) {
-    let str = "";
-    for(let i = 0; i<k; i++){
-        str += my_string;
-    }
-    return str;
-}
